d3b87fd6d4cf8a44c68c3db55716e874aada67e1
TylerAskew <taskew2299@gmail.com>
2015-04-11 22:36:04 -0400

    Added swipe gestures to parties within party list.

--- www/js/party_list.js
+++ www/js/party_list.js
@@ -18,17 +18,31 @@ function failedLocation(error) {
 */
 
 
-$$('div', 'dl').tap(() {
-	this.style('color', 'red');
-});
-
-
 // loading all local party data from Parse
 function getPartyData() {
 
 	// querying all party data
 	var Party = Parse.Object.extend("Party");
 	var party_query = new Parse.Query(Party);
+
+	// creating a gesture recognizer
+	var gestureHandler = new Hammer.Manager(document.getElementById('party_list'));
+	gestureHandler.add( new Hammer.Swipe({ event: 'swipeleft' }) );
+
+	// specifying animations for gesture
+	gestureHandler.on("swipeleft", function(ev) {
+		$("#" + ev.target.id).animate({ opacity: 0.25, right: '+=500' } , "400", 
+			function() {
+     					$('#party_list').animate(
+     						{ opacity: 0, right: '+=500' } , "800", 
+     						function() {
+     							document.body.innerHTML = "";
+     							loadPartyPictures(ev.target.id)
+     						}
+     					);
+     		}
+     	);
+	});
 
 	// iterating through all parties and modifying html
 	party_query.find({
@@ -45,13 +59,26 @@ function getPartyData() {
 				amount_donated.appendChild(amount_content);
 				party.appendChild(party_title);
 				party.appendChild(amount_donated);
+				party.setAttribute("class", "party");
+				party.setAttribute("id", results[i].id);
+				party.style.position = "relative";
+				document.getElementById("party_list").style.position = "relative";
 				document.getElementById("party_list").appendChild(party);
 			}
 		}, 
 
 		error : function(error) {
-			alert("Error fetching party information.");
+			alert("ERROR: " + error.code + ", " + error.message);
 		}
 	});
+
+}
+
+// loading all pictures for the specified party
+function loadPartyPictures(party_id) {
+
+	// querying all party data
+	var PartyPhoto = Parse.Object.extend("PartyPhoto");
+	var photo_query = new Parse.Query(Party);
 
 }
